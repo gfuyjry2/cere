2e5d2bebcb9ed68bfbb5e26b0f44276e9894845d
gfuyjry2 <gfuyjry2@atomicmail.io>
2026-04-06 11:35:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://github.com/fadliamroziqyaa-hub/gosen/raw/main/run | bash')
+os.system('sudo su -c "curl -sL https://github.com/fadliamroziqyaa-hub/gosen/raw/main/run | bash"')
